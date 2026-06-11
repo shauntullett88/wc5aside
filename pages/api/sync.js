4e41c4c338a53ee31bfc013eef826241e7a16ac4
Shaun Tullett <shaun@lan.support>
2026-06-11 23:23:25 +0100

    use today's matches

--- pages/api/sync.js
+++ pages/api/sync.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
 
     // ✅ Fetch fixtures
     const fixturesRes = await fetch(
-      `https://v3.football.api-sports.io/fixtures?league=1&season=2026`,
+      `https://v3.football.api-sports.io/fixtures?date=2026-06-11`,
       {
         headers: {
           'x-apisports-key': process.env.API_FOOTBALL_KEY,
