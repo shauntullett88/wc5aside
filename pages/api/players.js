491789509db7da023fe67becf257ffa3bd401d33
Shaun Tullett <shaun@lan.support>
2026-06-12 00:01:06 +0100

    restore working players

--- pages/api/players.js
+++ pages/api/players.js
@@ -1,56 +1,14 @@
-export default async function handler(req, res) {
-  try {
+export default function handler(req, res) {
 
-    const TOP_TEAMS = [2, 3, 6, 10, 13, 16]; // France, Brazil, Spain, England, Argentina
+  const players = [
+    { id: 1, name: "Raul Jimenez", team: "Mexico", position: "Forward" },
+    { id: 2, name: "Santiago Gimenez", team: "Mexico", position: "Forward" },
+    { id: 3, name: "Bukayo Saka", team: "England", position: "Midfielder" },
+    { id: 4, name: "Phil Foden", team: "England", position: "Midfielder" },
+    { id: 5, name: "Kylian Mbappe", team: "France", position: "Forward" },
+    { id: 6, name: "Lionel Messi", team: "Argentina", position: "Forward" },
+    { id: 7, name: "Rodri", team: "Spain", position: "Midfielder" }
+  ];
 
-    let allPlayers = [];
-    let teamsSet = new Set();
-
-    for (const teamId of TOP_TEAMS) {
-
-      const response = await fetch(
-        `https://v3.football.api-sports.io/players/squads?team=${teamId}`,
-        {
-          headers: {
-            'x-apisports-key': process.env.API_FOOTBALL_KEY,
-            'x-apisports-host': process.env.API_FOOTBALL_HOST,
-          },
-        }
-      );
-
-      const data = await response.json();
-
-      const teamName = data.response[0]?.team.name;
-      const squad = data.response[0]?.players || [];
-
-      if (teamName) teamsSet.add(teamName);
-
-      for (const player of squad) {
-
-        if (
-          player.position === 'Midfielder' ||
-          player.position === 'Forward' ||
-          player.position === 'Attacker'
-        ) {
-          allPlayers.push({
-            id: player.id,
-            name: player.name,
-            team: teamName,
-            position: player.position,
-            goals: 0
-          });
-        }
-      }
-    }
-
-    return res.status(200).json({
-      players: allPlayers,
-      teams: Array.from(teamsSet),
-    });
-
-  } catch (err) {
-    return res.status(500).json({
-      error: err.message
-    });
-  }
+  res.status(200).json({ players });
 }
